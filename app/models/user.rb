659ac98eb3a4b877f9a62774186112e7f728e4e3
Ansh Chaturvedi <anshchaturvedi@Anshs-MacBook-Pro.local>
2022-04-18 10:11:48 -0400

    added user creation and logout functionality

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# email:string
+# password_digest:string
+
+# password:string virtual
+# password_confirmation: string virtual
+
+class User < ApplicationRecord
+  has_secure_password
+  validates :email,
+  format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Email invalid"  },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # defining the about section
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   # Defines the root path route ("/")
   root to: "main#index"
 end
