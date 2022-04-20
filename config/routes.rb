8994e072150d4d0d7ddf02d09a6b2f834d95cf7b
Ansh Chaturvedi <anshchaturvedi@Anshs-MacBook-Pro.local>
2022-04-20 13:14:17 -0400

    tweaked and finished twitter connection functionality

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
   # Defines the root path route ("/")
   root to: "main#index"
 end
